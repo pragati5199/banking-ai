989e163dc6415c0697a2b82b45cb4e0147a924f6
Prashant Saxena <your-email@gmail.com>
2026-05-16 18:18:17 +0530

    Add pagination to GET /accounts endpoint

--- src/main/java/com/banking/AccountController.java
+++ src/main/java/com/banking/AccountController.java
@@ -2,12 +2,13 @@ package com.banking;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
@@ -21,8 +22,8 @@ public class AccountController {
 
 
     @GetMapping
-    public List<AccountResponseDTO> getAllAccounts(){
-        return accountService.getAllAccounts();
+    public Page<AccountResponseDTO>getAllAccounts(Pageable page){
+        return accountService.getAllAccounts(page);
     }
 
     @PostMapping
--- src/main/java/com/banking/AccountService.java
+++ src/main/java/com/banking/AccountService.java
@@ -2,12 +2,13 @@ package com.banking;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -17,12 +18,10 @@ public class AccountService {
     @Autowired
     AccountRepository accountRepository;
 
-    public List<AccountResponseDTO> getAllAccounts(){
+    public Page<AccountResponseDTO> getAllAccounts(Pageable pageable){
 
-            return accountRepository.findAll()
-                    .stream()
-                    .map(AccountMapper::toResponseDTO)
-                    .toList();
+            return accountRepository.findAll(pageable)
+                    .map(AccountMapper::toResponseDTO);
 
     }
 
